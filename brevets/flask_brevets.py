62f873ca68f18106d29980d82d2c6f3478f32598
zbower1uo <32285228+zbower1uo@users.noreply.github.com>
2017-10-22 21:35:06 -0700

    update

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -46,19 +46,16 @@ def page_not_found(error):
 ###############
 @app.route("/_calc_times")
 def _calc_times():
-    """
-    Calculates open/close times from miles, using rules
-    described at https://rusa.org/octime_alg.html.
-    Expects one URL-encoded argument, the number of miles.
-    """
-    app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
-    app.logger.debug("km={}".format(km))
-    app.logger.debug("request.args: {}".format(request.args))
-    # FIXME: These probably aren't the right open and close times
-    # and brevets may be longer than 200km
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat)
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat)
+
+    begin_time = request.args.get('begin_time', "00:00")
+    begin_date = request.args.get('begin_date', "2017-01-01")
+    brevet_dist_km = request.args.get('brevet_dist_km', 1000, type=int)
+    date_time = begin_date  + begin_time   
+
+
+    open_time = acp_times.open_time(km, brevet_dist_km,date_time)
+    close_time = acp_times.close_time(km, brevet_dist_km,date_time)
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
